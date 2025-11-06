d4fe1312a9fe1147fc3ff719dd607c5d0a7310a2
Aryan Subhash <aryansubhash19@gmail.com>
2025-11-06 07:08:39 +0000

    Create callback layout.tsx

---
+++ app/auth/callback/layout.tsx
@@ -0,0 +1,5 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+export const fetchCache = 'force-no-store';
+export const runtime = 'nodejs';
+export default function CallbackLayout({ children }: { children: React.ReactNode }) { return <>{children}</>; }
